fa51c50bd7d3296128fd78edcab80830aaf28edd
Diego Nonato <nonatodiego@hotmail.com>
2025-02-06 09:25:57 -0300

    :sparkles: feat: Criando página de login

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className="container">
+            </div>
+        </header>
+    )
+}
