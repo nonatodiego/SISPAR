dddba4987bb9f840e1ee3b1eccbde7b1bdf79cad
Diego Nonato <nonatodiego2014@gmail.com>
2025-02-12 09:02:41 -0300

    :sparkles: feat: adiciona interatividade ao cabeçalho com animações e melhorias de estilo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from './header.module.scss';
 import menuIcon from '../../assets/menuIcon.png'
 import profileIcon from '../../assets/profileIcon.png'
@@ -9,37 +10,54 @@ import sairIcon from '../../assets/sairIcon.png'
 
 
 export default function Header() {
+    const [isExpanded, setIsExpanded] = useState(false);
 
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${isExpanded ? styles.expanded : ""}`}
+        onMouseEnter={() => setIsExpanded(true)}
+        onMouseLeave={() => setIsExpanded(false)}>
+
             <div className={styles.iconMenu}>
                 <img src={menuIcon} alt="" />
             </div>
+                
             <nav>
                 <ul>
                     <li>
-                        <img src={profileIcon} alt="Icone de perfil" />
+                        <span className={`${styles.navIcons} ${styles.perfilIcon}`}>
+                            <img src={profileIcon} alt="Icone de perfil" />
+                        </span>
+                        {isExpanded && <span>Perfil</span>}
                     </li>
                     <li>
-                        <img src={homeIcon} alt="Icone de Home" />
+                        <span  className={styles.navIcons}>
+                            <img src={homeIcon} alt="Icone de Home" />
+                        </span>
+                        {isExpanded && <span>Início</span>}
                     </li>
                     <li>
-                        <img src={reembolsosIcon} alt="Icone de Reembolso" />
+                        <span className={styles.navIcons}>
+                            <img src={reembolsosIcon} alt="Icone de Reembolso" />
+                        </span>
+                        {isExpanded && <span>Reembolsos</span>}
                     </li>
                     <li>
-                        <img src={pesquisaIcon} alt="Icone de Pesquisa" />
+                        <span className={styles.navIcons}>
+                            <img src={pesquisaIcon} alt="Icone de Pesquisa" />
+                        </span>
+                        {isExpanded && <span>Análises</span>}
                     </li>
                     <li>
-                        <img src={historicoIcon} alt="Icone de Histórico" />
+                        <span className={styles.navIcons}>
+                            <img src={historicoIcon} alt="Icone de Histórico" />
+                        </span>
+                        {isExpanded && <span>Histórico</span>}
                     </li>
-
                 </ul>
             </nav>
             <div className={styles.iconSair}>
                 <img src={sairIcon} alt="" />
             </div>
-
-
         </header>
     )
 }
