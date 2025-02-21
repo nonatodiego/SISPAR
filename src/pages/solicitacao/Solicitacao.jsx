6a9c73dbf8351147aeacfb5865a434a751a97b8c
Diego Nonato <nonatodiego2014@gmail.com>
2025-02-21 19:30:27 -0300

    :sparkles: feat: adiciona novos campos e tabela à página de solicitação, atualiza estilos e layout

--- src/pages/solicitacao/Solicitacao.jsx
+++ src/pages/solicitacao/Solicitacao.jsx
@@ -3,6 +3,8 @@ import NavBar from '../../components/navbar/NavBar.jsx';
 import styles from "./Solicitacao.module.scss";
 import Home from '../../assets/iconHome.png'
 import Seta from '../../assets/iconSeta.png'
+import Lixeira from '../../assets/lixeira.png'
+import Motivo from '../../assets/motivo.png'
 
 export default function Solicitacao() {
   return (
@@ -49,10 +51,120 @@ export default function Solicitacao() {
                     <option value="">Selecionar</option>
                     <option value="">Alimentação</option>
                     <option value="">Combustivel</option>
+                    <option value="">Condução</option>
+                    <option value="">Estacionamento</option>
+                    <option value="">Viagem Admin.</option>
+                    <option value="">Viagem Operacional</option>
+                    <option value="">Eventos de representação</option>
                   </select>
+                </div>
+
+                <div className={styles.centroDeCusto}>
+                  <label htmlFor="">Centro de custo</label>
+                  <select name="" id="">
+                    <option value=""> Selecionar</option>
+                    <option value="">
+                      1100109002 - FIN CONTROLES INTERNOS MTZ
+                    </option>
+                    <option value="">
+                      1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ
+                    </option>
+                    <option value="">1100110101 - FIN CONTABILIDADE MTZ</option>
+                  </select>
+                </div>
+
+                <div className={styles.ordem}>
+                  <label htmlFor="">Ord. Int.</label>
+                  <input type="number" />
+                </div>
+                <div className={styles.divisoes}>
+                  <label htmlFor="">Div</label>
+                  <input type="number" />
+                </div>
+                <div className={styles.pep}>
+                  <label htmlFor="">PEP</label>
+                  <input type="number" />
+                </div>
+
+                <div className={styles.moeda}>
+                  <label htmlFor="">Moeda</label>
+                  <select name="" id="">
+                    <option value="">Selecionar</option>
+                    <option value="">BRL</option>
+                    <option value="">ARS</option>
+                    <option value="">USD</option>
+                  </select>
+                </div>
+
+                <div className={styles.distancia}>
+                  <label htmlFor="">Dist / Km</label>
+                  <input type="text" />
+                </div>
+
+                <div className={styles.valorKm}>
+                  <label htmlFor="">Valor / Km</label>
+                  <input type="number" />
+                </div>
+
+                <div className={styles.valorFaturado}>
+                  <label htmlFor="">Valor / Faturado</label>
+                  <input type="number" />
+                </div>
+
+                <div className={styles.despesas}>
+                  <label htmlFor="">Despesas</label>
+                  <input type="number" />
+                </div>
+
+                <div className={styles.botoes}>
+                  <button className={styles.btnSalvar}>+ Salvar</button>
+                  <button className={styles.btnDel}>Del</button>
                 </div>
               </div>
             </form>
+
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Colaborador(a)</th>
+                  <th>Empresa</th>
+                  <th>Nº Prest.</th>
+                  <th>Data</th>
+                  <th>Motivo</th>
+                  <th>Tipos de despesa</th>
+                  <th>Ctr. Custo</th>
+                  <th>Ord. Int.</th>
+                  <th>Div.</th>
+                  <th>PEP</th>
+                  <th>Moeda</th>
+                  <th>Dist. Km</th>
+                  <th>Val. Km</th>
+                  <th>Val. Faturado</th>
+                  <th>Despesa</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td><img src={Lixeira} alt="" /></td>
+                  <td>Jefin</td>
+                  <td>Mototaxi</td>
+                  <td>24</td>
+                  <td>21/02/2025</td>
+                  <td><img src={Motivo} /></td>
+                  <td>Alimentação</td>
+                  <td>200.00</td>
+                  <td>1</td>
+                  <td>0</td>
+                  <td>nada</td>
+                  <td>BRL</td>
+                  <td>20km</td>
+                  <td>0.50</td>
+                  <td>600.00</td>
+                  <td>1500.00</td>
+                </tr>
+              </tbody>
+            </table>
           </main>
         </div>
       </div>
